2e88d964efd54b829208cb72a164f4b9a6766e47
Sam Lin <timefallensam@gmail.com>
2026-02-26 12:03:12 -0600

    Completed Express Server project

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
-// import files and packages up here
 
+var express = require('express')
+var morgan  = require('morgan')
+var data = require('./data.json')
 
-// create your express server below
-var app;
+var app = express();
+app.use(morgan('dev'));
 
-// add your routes and middleware below
+app.get('/', function(req, res) {
+    res.status(200).send("It's working");
+});
 
-// finally export the express application
+app.get('/data', function(req, res) {
+    res.status(200).send(data);
+});
+
 module.exports = app;
